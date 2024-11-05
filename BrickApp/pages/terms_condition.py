fa93fecd713bf6b346271c8c44a833631ed269e6
Daniele.Antonucci <daniele.antonucci@eurac.edu>
2024-11-05 10:49:51 +0100

    update terms

--- BrickApp/pages/terms_condition.py
+++ BrickApp/pages/terms_condition.py
@@ -1,7 +1,5 @@
 import dash
-from dash import html, dcc
 import  dash_mantine_components as dmc
-from dash_iconify import DashIconify
 
 
 dash.register_page(__name__, path='/terms&condition')
@@ -23,24 +21,6 @@ lice_4 = dmc.ListItem("1. Redistributions of source code must retain the above c
 lice_5 = dmc.ListItem("2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.", mt=10)
 lice_6 = dmc.ListItem("3. All advertising materials mentioning features or use of this software must display the following acknowledgement: This product includes software developed by Eurac research and Politecnico of Turin.", mt=10)
 lice_7 = dmc.ListItem("4. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.", mt=10)
-
-
-
-# 1. Redistributions of source code must retain the above copyright notice, this
-#    list of conditions and the following disclaimer.
-
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution.
-
-# 3. All advertising materials mentioning features or use of this software must
-#    display the following acknowledgement:
-#      This product includes software developed by Eurac research and Politecnico of Turin.
-
-# 4. Neither the name of the copyright holder nor the names of its
-#    contributors may be used to endorse or promote products derived from
-#    this software without specific prior written permission.
-
 lice_8 = "THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDER 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, \
     PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; \
         OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, \
